8a7f4982a623f9282e23d747f7114a3b9f03c04d
Theo Seguin <theo.seguin@student-cs.fr>
2023-07-20 23:41:22 +0200

    can visualize table

--- Environement.py
+++ Environement.py
@@ -35,7 +35,6 @@ class player():
         self.name = name
         self.__cards = []
         self.chip_stack = chip_stack
-        self.bet = 0
     
     def show_card(self):
         print(self.__cards)
@@ -46,21 +45,76 @@ class player():
     def get_cards(self):
         return self.__cards
 
-    
-class board():
-    # Objet possedant : pot + cartes + défausse
-    def __init__(self):
-        self.cards = []
-        self.bin = []
-        self.pot = 0
-
 
 class table():
-    #plusieurs joueurs, un pot, un board, un deck,..
-    def __init__(self) -> None:
-        pass
+    #plusieurs joueurs, un pot, un board, un deck, le bouton
+    def __init__(self):
+        self.players = {}
+        self.dealer = 0
+        self.board = []
+        self.bin = []
+        self.pot = 0
+        self.nb_players=2
+        self.players_bet = {}
+        self.player_to_speak = self.get_first_to_talk()
+    
+    def add_players(self, players:list[player]):
+        for i, p in enumerate(players):
+            self.players[i] = p
+            self.players_bet[i] = 0
+            self.nb_players = len(self.players)
 
-class game():
-    # Objet possédant les valeur des blindes, les prix, ect
-    def __init__(self) -> None:
-        pass
+    def remove_player(self):
+        pass ## TO COMPLETE
+
+    def get_first_to_talk(self):
+        return (self.dealer + 3)%self.nb_players
+    
+    def is_bet_over(self):
+        pass ##TO COMPLETE
+
+    def bet_from_player(self, player_id, bet_amount):
+        self.players_bet[player_id] = bet_amount
+        self.players[player_id].chip_stack -= bet_amount
+    
+    def next(self):
+        self.player_to_speak += 1
+        self.player_to_speak%self.nb_players
+
+
+    def print_table(self):
+        board_length = 5 + 15*self.nb_players
+        print('+'+board_length*'-'+'+')
+        # print(f'| BB : {self.BB:<boad_lenght-6}|')   PEUT ETRE FAIRE UNE CLASSE "GAME" POUR AVOIR self.BB
+        print('|'+board_length*' '+'|')
+        print('|'+f'POT : {self.pot}'.center(board_length, ' ')+'|')
+        print('|'+board_length*' '+ '|')
+        print('|'+f'CARDS : {self.board}'.center(board_length, ' ')+'|')
+        print('|'+ board_length*' '+'|')
+        print('|     ', end='')
+        for i in range(self.nb_players): 
+            print(f'{self.players_bet[i]}'.center(10, ' ')+5*' ', end='') 
+        print('|')
+        print('|'+board_length*' '+'|')
+        print('|     ', end='')
+        for i in range(self.nb_players):
+            print(f'{self.players[i].name}'.center(10, ' ')+5*' ', end='')
+        print('|')
+        print('|     ', end='')
+        for i in range(self.nb_players):
+            print(f'{self.players[i].chip_stack}'.center(10, ' ')+5*' ', end='')
+        print('|')
+        gap_length = 9 + board_length*self.player_to_speak//self.nb_players - self.player_to_speak
+        print('|'+gap_length*' '+'^'+(board_length-gap_length-1)*' '+'|')
+        print('+'+board_length*'-'+'+')
+    
+
+if __name__ == "__main__":
+
+    P1 = player(name="Alice", chip_stack=200)
+    P2 = player(name="Bob", chip_stack=200)
+    P3 = player(name="Carl", chip_stack=100)
+    T = table()
+    T.add_players([P1, P2, P3, player()])
+    T.player_to_speak = 2
+    T.print_table()
--- Test.py
+++ Test.py
@@ -1,19 +1,36 @@
 from  Environement import *
 
+BB = 10
+player_stack = 300
+nb_round = 5
+nb_players = 2
+
+P1 = player(name="Alice", chip_stack=player_stack)
+P2 = player(name="Bob", chip_stack=player_stack)
+
+table = table(nb_players=2)
+table.add_players([P1, P2])
+
 D = deck()
-P1 = player(name="Alice")
 
-# print(D.stack)
-# print(D.distribuated_cards)
-# print('----------------------')
-# D.shuffle()
-# dist_stacks = D.distribute(3, 10)
-# print(dist_stacks)
-# print(D.stack)
-# print(D.distribuated_cards)
-# print('-----------------------')
-# D.reset()
-# print(D.stack)
-# print(D.distribuated_cards)
+for _ in range(nb_round):
 
-P1.show_card()
+    ## Distibution des cartes
+    D.shuffle()
+    player_cards = D.distribute(nb_stacks=nb_players, nb_cards=2)
+    for i, p in enumerate(table.players):
+        p.set_cards(player_cards[i])
+    
+    ## Blindes & Ante
+    id_SB = (table.get_first_to_talk() + 1)%table.nb_players
+    id_BB = (id_SB + 1)%table.nb_players
+    table.bet_from_player(player_id=id_SB, bet_amount=BB//2)
+    table.bet_from_player(player_id=id_BB, bet_amount=BB)
+    
+    ## Actions de chaque joueurs
+    
+
+
+
+
+
